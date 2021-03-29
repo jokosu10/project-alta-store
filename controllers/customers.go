2b5a1cf1bae04058de469c0180c42debb85d9b4b
Joko Susilo <susilo.j8@gmail.com>
2021-03-29 08:10:30 +0700

    Add feature register user

---
+++ controllers/customers.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"net/http"
+	"project-alta-store/lib/database"
+	"project-alta-store/models"
+
+	"github.com/labstack/echo"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func RegisterUserController(c echo.Context) error {
+	var customerModel models.Customers_register
+
+	if e := c.Bind(&customerModel); e != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+			"code":    400,
+			"message": "Fail insert data",
+			"status":  e.Error(),
+		})
+	}
+
+	// cek validation struct
+	if e := models.Validate.Struct(customerModel); e != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+			"code":    400,
+			"message": "Error registration user",
+			"status":  e.Error(),
+		})
+	}
+
+	// Generate "hash" to store from user password
+	hash, _ := bcrypt.GenerateFromPassword([]byte(customerModel.Password), bcrypt.DefaultCost)
+
+	var customer models.Customers
+	customer.Username = customerModel.Username
+	customer.Email = customerModel.Email
+	customer.Password = string(hash)
+
+	_, err := database.InsertCustomers(customer)
+
+	// cek before insert to database
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, map[string]interface{}{
+			"code":    500,
+			"message": "Error registration user",
+			"status":  err.Error(),
+		})
+	}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"code":    200,
+		"message": "success register user",
+		"status":  "success",
+	})
+}
--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,20 @@
 package routes
 
-import(
+import (
 	"project-alta-store/controllers"
+
 	"github.com/labstack/echo"
 )
 
-func Start() *echo.Echo{
+func Start() *echo.Echo {
 	e := echo.New()
-	e.GET("/products",controllers.GetProductsController)
-	e.POST("/products",controllers.CreateProductsController)
+
+	// route auth
+	e.POST("/register", controllers.RegisterUserController)
+
+	// route products
+	e.GET("/products", controllers.GetProductsController)
+	e.POST("/products", controllers.CreateProductsController)
 
 	return e
 }
